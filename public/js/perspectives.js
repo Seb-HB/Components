c3c50cc2b5b18eccfa9036ed8c78534ff52f7628
Seb-HB <sfouvet@stagiaire-humanbooster.com>
2021-06-18 16:17:49 +0200

    updtae perspectives component

--- public/js/perspectives.js
+++ public/js/perspectives.js
@@ -1,6 +1,6 @@
 let diffX, diffY;
-document.querySelector("#perspectives").addEventListener('mousemove', function(e){
+document.querySelector("#perspectives div").addEventListener('mousemove', function(e){
     diffX= (0.55+((window.screen.width/2)-e.clientX)/(window.screen.width/2))*100;
     diffY= (-0.15+((window.screen.height/2)-e.clientY)/(window.screen.height/2))*100;
-    document.querySelector("#perspectives").style.perspectiveOrigin=`${diffX}% ${diffY}%`;
+    document.querySelector("#perspectives div").style.perspectiveOrigin=`${diffX}% ${diffY}%`;
 })
