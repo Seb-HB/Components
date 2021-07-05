f5a3057765107cddddcaf7a02c2fab8227d4f4bd
Fouvet Sébastien <fouvet.developpeur@gmail.com>
2021-07-05 03:51:57 +0200

    upgrade intro scroll with text animation

--- public/js/introSwitch.js
+++ public/js/introSwitch.js
@@ -1,18 +1,8 @@
 let anim=document.querySelector('#sf_bg-animation');
+let animText=document.querySelector('.sf_bg-anim-text');
 let winHeight=window.innerHeight;
 let scrollUp;
-console.log(anim);
 
-//vérifie la direction du scroll. scrollUp sera vrai si on remonte
-const scrollDirection=function(){
-    scrollUp=false;
-    let initScroll=window.scrollY;
-    setTimeout(function(){
-        let finishScroll=window.scrollY;
-        (initScroll-finishScroll)>=0 ? scrollUp=true : scrollUp=false;
-        console.log('diff: '+(initScroll-finishScroll),scrollUp );
-    },5);
-}
 
 window.onscroll = function(e) {
     let initScroll=window.scrollY;
@@ -25,16 +15,16 @@ window.onscroll = function(e) {
         }else if (deltaScroll<0){
             scrollUp=false;
         }
-        // (initScroll-finishScroll)>=0 ? scrollUp=true : scrollUp=false;
-        console.log('diff: '+(deltaScroll),scrollUp );
     },5);
-    console.log(scrollUp);
-    console.log('scroll : '+ window.scrollY, 'height: '+winHeight);
     if (window.scrollY<winHeight && scrollUp==true){
         // document.querySelector('body').scrollTop=0;
         window.scrollTo({top:0, behavior: 'smooth'});
         //indispensable pour pouvoir rescroller vers le bas
         scrollUp=false;
+        //suppresion de la classe, reflow event puis remise en place de la classe pour relancer l'animation du texte
+        animText.className='';
+        void animText.offsetWidth;
+        animText.className='sf_bg-anim-text';
     }
 
 };
