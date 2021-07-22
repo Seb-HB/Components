bee0e1b43f115ccc986daa819400e27389835c31
Fouvet Sébastien <fouvet.developpeur@gmail.com>
2021-07-22 23:40:50 +0200

    add taglinks to all sections

--- public/js/perspectives.js
+++ public/js/perspectives.js
@@ -1,6 +1,6 @@
 let diffX, diffY;
-document.querySelector("#perspectives div:last-child").addEventListener('mousemove', function(e){
+document.querySelector("#perspectives div:nth-child(2)").addEventListener('mousemove', function(e){
     diffX= (0.55+((window.screen.width/2)-e.clientX)/(window.screen.width/2))*100;
     diffY= (-0.15+((window.screen.height/2)-e.clientY)/(window.screen.height/2))*100;
-    document.querySelector("#perspectives div:last-child").style.perspectiveOrigin=`${diffX}% ${diffY}%`;
+    document.querySelector("#perspectives div:nth-child(2)").style.perspectiveOrigin=`${diffX}% ${diffY}%`;
 })
