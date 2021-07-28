05201c5bd1be1a9aa030da4a0fb13f886435b931
Fouvet Sébastien <fouvet.developpeur@gmail.com>
2021-07-29 00:46:13 +0200

    minor updates

--- public/js/perspectives.js
+++ public/js/perspectives.js
@@ -1,6 +1,6 @@
 let diffX, diffY;
 document.querySelector("#perspectives div:nth-child(2)").addEventListener('mousemove', function(e){
-    diffX= (0.55+((window.screen.width/2)-e.clientX)/(window.screen.width/2))*100;
-    diffY= (-0.15+((window.screen.height/2)-e.clientY)/(window.screen.height/2))*100;
+    diffX= (0.55+((window.screen.width/2)-e.clientX)/(window.screen.width/2)*1.3)*100;
+    diffY= (-0.15+((window.screen.height/2)-e.clientY)/(window.screen.height/2)*1.3)*100;
     document.querySelector("#perspectives div:nth-child(2)").style.perspectiveOrigin=`${diffX}% ${diffY}%`;
 })
