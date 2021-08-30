a7ffc8fce8e75d01240ae4e0d241da74965cd2ad
Fouvet Sébastien <fouvet.developpeur@gmail.com>
2021-08-31 01:44:44 +0200

    autoload

--- public/index.php
+++ public/index.php
@@ -1,7 +1,23 @@
 <?php
+spl_autoload_register(function ($class){
 
-require 'controllers/HomeController.php';
-require 'controllers/AboutController.php';
+    $folders=[
+        'controllers/',
+        'models/',
+        'models/managers/'
+    ];
+    foreach($folders as $folder){
+        if (file_exists($folder.$class.'.php')){
+            require_once $folder.$class.'.php';
+            break;
+        }
+        // else{
+        //     echo($class.' n\'a pas été trouvé dans le répertoire '.$folder.'<br>');
+        //     echo($folder.$class.'.php <br>');
+        // }
+    }
+
+});
 
 
 if (isset($_GET['page'])){
