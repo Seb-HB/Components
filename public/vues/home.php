910c03e067de2751f461fcdc1f0cf1c8a5bd219c
Fouvet Sébastien <fouvet.developpeur@gmail.com>
2021-08-31 00:46:40 +0200

    add autoload

--- public/vues/home.php
+++ public/vues/home.php
@@ -34,7 +34,7 @@
     ?>
 
     <script src="js/introSwitch.js"></script>
-    <script src="js/perspectives.js"></script>>
+    <script src="js/perspectives.js"></script>
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
     integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
     crossorigin=""></script>
