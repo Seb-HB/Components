ba38a0a99ff2e99e9136302da79bbc2e670ec366
Fouvet Sébastien <fouvet.developpeur@gmail.com>
2022-01-20 17:40:31 +0100

    Add gsap librairy

--- public/vues/about.php
+++ public/vues/about.php
@@ -197,5 +197,6 @@
         require 'vues/partials/footer.php';
     ?>
     <script src="js/about.js"></script>>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
 </body>
 </html>
