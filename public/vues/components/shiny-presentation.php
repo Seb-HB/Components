488306853137c5b7a2d1623b10dd88b0e14f920e
Fouvet Sébastien <fouvet.developpeur@gmail.com>
2022-02-13 10:25:12 +0100

    MAJ Présentation

--- public/vues/components/shiny-presentation.php
+++ public/vues/components/shiny-presentation.php
@@ -1,10 +1,8 @@
 <section id="shiny">
     <h1 class="sf-shiny">Développeur Web FullStack junior</h1>
-    <p>Diplômé RNCP webmaster / webdesigner mention très bien en 2017, je n'ai malheureusement que peu eu l'occasion de pratiquer le développement web.</p>
-    <p>J'ai décidé de me former à nouveau en 2021 pour me mettre à jour et complèter mes précédents acquis.</p>
-    <p>Je suis donc en formation certifiante pour le titre RNCP de développeur web et web mobile, formation qui prendra fin au terme d'un stage en entreprise du 2/11/2021 au 10/01/2022.</p>
-    <p>Ce site est donc une vitrine pour montrer mes idées et mes réalisations sous forme d'un portfolio de composants réutilisables avec un fichier SASS individuel.</p>
+    <p>Diplômé RNCP webmaster / webdesigner mention très bien en 2017, puis Développeur Web et Web Mobile en 2022.</p>
+    <p>Ce site est une vitrine pour montrer mes idées et mes réalisations sous forme d'un portfolio de composants réutilisables avec un fichier SASS individuel.</p>
     <p>Ces composants utilisent bien-sûr HTML/CSS (Sass), mais aussi du JS (vanilla, angular) et du PHP (POO), sans aucune librairie externe (sauf mention contraire).</p>
-    <p>Veuillez noter que ce site est encore en construction, qu'il manque encore des pages. il est aussi en perpetuelle évolution au gré de mes idées.</p>
+    <p>Veuillez noter que ce site est encore en construction, qu'il manque encore des pages. il est aussi en perpetuelle évolution au gré de mes envies.</p>
 
 </section>
