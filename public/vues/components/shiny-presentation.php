37ca718d67c9b04396971aed2ff9914e971a45da
Fouvet Sébastien <fouvet.developpeur@gmail.com>
2022-02-18 14:44:00 +0100

    Update presentation content

--- public/vues/components/shiny-presentation.php
+++ public/vues/components/shiny-presentation.php
@@ -1,8 +1,7 @@
 <section id="shiny">
     <h1 class="sf-shiny">Développeur Web FullStack junior</h1>
     <p>Diplômé RNCP webmaster / webdesigner mention très bien en 2017, puis Développeur Web et Web Mobile en 2022.</p>
-    <p>Ce site est une vitrine pour montrer mes idées et mes réalisations sous forme d'un portfolio de composants réutilisables avec un fichier SASS individuel.</p>
+    <p>Ce site est une vitrine pour montrer mes idées et mes réalisations sous forme d'un portfolio de composants avec fichier SASS individuel.</p>
     <p>Ces composants utilisent bien-sûr HTML/CSS (Sass), mais aussi du JS (vanilla, angular) et du PHP (POO), sans aucune librairie externe (sauf mention contraire).</p>
-    <p>Veuillez noter que ce site est encore en construction, qu'il manque encore des pages. il est aussi en perpetuelle évolution au gré de mes envies.</p>
-
+    <p>Veuillez noter que ce site est encore en construction, il est aussi en perpetuelle évolution au gré de mes envies et de ma curiosité.</p>
 </section>
